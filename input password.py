c7f5da9ae6392ae6f8351db436d0dcc668d6efd9
Sam1521 <70865013+sam15211521@users.noreply.github.com>
2021-05-19 08:34:53 -0500

    changed input to input passowrd add pass

---
+++ input password.py
@@ -0,0 +1,21 @@
+username = input("Type username ")
+password = ""
+if username == "sam1521":
+    password = input("Type password ")
+    if password == "1521":
+        print ("Welcome Sam Hendrix")
+    else:
+        print ("invalad password")
+else:
+    print("invalad user name")
+
+    
+
+
+
+question = input("are you there? y/n   ")
+if question == "y":
+    print ("hello")
+else:
+    print("goodbye")
+    
