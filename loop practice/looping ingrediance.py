377894dc01480be17232cb2af824187f9f9a81e0
Sam1521 <70865013+sam15211521@users.noreply.github.com>
2021-05-21 15:02:46 -0500

    added practice on while loops in ingre

---
+++ loop practice/looping ingrediance.py
@@ -0,0 +1,8 @@
+ingredients = ["milk", "sugar", "vanilla extract", "dough", "chocolate"]
+#insert while loop
+length = len(ingredients)
+i = 0
+while i <length:
+    print (ingredients[i])
+    i += 1
+    
