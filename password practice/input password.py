68252bfd2eadc1883485ad60fddf58e152623816
Sam1521 <70865013+sam15211521@users.noreply.github.com>
2021-05-24 11:43:55 -0500

    added an input if you git the correct password

--- password practice/input password.py
+++ password practice/input password.py
@@ -1,11 +1,14 @@
 username = input("Type username ")
 password = ""
+login_yes = ""
 if username == "sam1521":
     password = input("Type password ")
     if password == "1521":
         print ("Welcome Sam Hendrix")
+        login_yes = "Agd12@4729yfm"
     else:
         print ("invalad password")
 else:
     print("invalad user name")
 
+print(login_yes)
