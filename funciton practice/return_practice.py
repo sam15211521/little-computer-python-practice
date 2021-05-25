e2f1fe7e253a0ff889fe824fccf9d1dffc88e32b
Sam1521 <70865013+sam15211521@users.noreply.github.com>
2021-05-25 10:24:02 -0500

    added a function to multiply using lists clunky

--- funciton practice/return_practice.py
+++ funciton practice/return_practice.py
@@ -1,11 +1,20 @@
-
-def multiplication(num=input("choose your number"), time=input("choose your multiplicator")):
+# a function to multiply a number by another number
+# does not work well, way to complicated and cannot use for larger numbers
+def multiplication(num, time):
     product = 0
-    times =range(len(time))
+    times =list(range(time))
     i =0
-    while i <=times:
-        product += num + num
-        i +1
-    return product
+    while i <= len(times)-1:
+        product += num
+        i +=1
+    print(product)
+#function for choosing what type of operation is to be done
+    
+cyt = choose_your_type =input("What type of operation do you want? ad, sub, mul, div: ")
+    
+if cyt == "mul":
+    multiplication(int(input("choose your number: ")), int(input("choose your multiplicator: ")))
+else:
+    print("not yet made")
+    
 
-print(int(range(len(3))))
