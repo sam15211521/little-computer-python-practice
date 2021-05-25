a4f648c6c9edd46d11b59364ab5ceb332e8ba1ac
Sam1521 <70865013+sam15211521@users.noreply.github.com>
2021-05-25 09:45:34 -0500

    added practice for function

---
+++ funciton practice/return_practice.py
@@ -0,0 +1,11 @@
+
+def multiplication(num=input("choose your number"), time=input("choose your multiplicator")):
+    product = 0
+    times =range(len(time))
+    i =0
+    while i <=times:
+        product += num + num
+        i +1
+    return product
+
+print(int(range(len(3))))
