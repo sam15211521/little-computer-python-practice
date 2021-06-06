a4577377766d0685e2fadad2d5ddfba5f1dd09af
sam15211521 <70865013+sam15211521@users.noreply.github.com>
2021-06-06 15:34:13 -0500

    added all fist function challanges

---
+++ codecademy scripts/Python Code Challenges -functions/1-Tenth Power/1-Tenth Power.py
@@ -0,0 +1,14 @@
+# takes a number and raises it to the 10th power
+# Write your tenth_power function here:
+
+def tenth_power(num):
+    ans = num **10
+    return ans
+
+# Uncomment these function calls to test your tenth_power function:
+print(tenth_power(1))
+# 1 to the 10th power is 1
+print(tenth_power(0))
+# 0 to the 10th power is 0
+print(tenth_power(2))
+# 2 to the 10th power is 1024
